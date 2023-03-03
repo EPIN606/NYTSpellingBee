2ae2fb283d0a27789873511127abde6016d11ab1
Enrique <epineda19@austincollege.edu>
2023-03-03 11:34:58 -0600

    Added JUnit 4

---
+++ src/test/java/cs380/NYTSpellingBee/AllTests.java
@@ -0,0 +1,10 @@
+package cs380.NYTSpellingBee;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({AppTest.class})
+public class AllTests {
+}
