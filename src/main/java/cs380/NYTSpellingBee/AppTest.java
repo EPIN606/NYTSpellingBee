159e582f91d29f548e7a87f12bfd0a9a68f1ff36
Enrique <epineda19@austincollege.edu>
2023-03-06 11:34:30 -0600

    Java 19

---
+++ src/main/java/cs380/NYTSpellingBee/AppTest.java
@@ -0,0 +1,138 @@
+package cs380.NYTSpellingBee;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testStartStage() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRoot() {
+		fail("Not yet implemented");	
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchClassOfQextendsApplicationStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLaunchStringArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testApplication() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartStage1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHostServices() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetParameters() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyPreloader() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUserAgentStylesheet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
